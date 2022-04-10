d0981d65c4fa363a9eb614d4736b06ab362d68d6
Elliot Courant <me@elliotcourant.dev>
2022-04-10 12:37:48 -0500

    fix: Making sure the KDFSHA512 matches Golang's behavior.

--- src/kdf.ts
+++ src/kdf.ts
@@ -1,20 +1,28 @@
 // KDFSHA512 is a key-derivation-function using SHA512 as the inner and outer hash.
-export async function KDFSHA512(salt: string, username: string, password: string): Promise<BigInt> {
-  const innerInput = `${ username }:${ password }`;
+import { hexToUint8Array, uint8ArrayToHex } from './buffer';
+
+export async function KDFSHA512(salt: Uint8Array, username: string, password: string): Promise<bigint> {
+  const innerInput = stringToUint8Array(`${ username }:${ password }`);
   const innerResult = await sumSHA512(innerInput);
 
-  const outerInput = `${ salt }${ innerInput }`;
+  let outerInput = new Uint8Array(salt.length + innerResult.length);
+  outerInput.set(salt, 0);
+  outerInput.set(innerResult, salt.length);
   const outerResult = await sumSHA512(outerInput);
 
-  return BigInt(`0x${ outerResult }`);
+  return BigInt(`0x${ uint8ArrayToHex(outerResult) }`);
+}
+
+export function stringToUint8Array(input: string): Uint8Array {
+  return Uint8Array.from(input, c => c.charCodeAt(0));
 }
 
 // sumSHA512 just takes the provided string and returns the sum of the SHA512 hash of that string.
-export async function sumSHA512(input: string): Promise<string> {
-  const outerResult = await crypto.subtle.digest('SHA-512', Uint8Array.from(decodeURI(encodeURIComponent(input)), c => c.charCodeAt(0)))
+export async function sumSHA512(input: Uint8Array): Promise<Uint8Array> {
+  const outerResult = await crypto.subtle.digest('SHA-512', input.buffer)
     .then(buf => Array.prototype.map.call(new Uint8Array(buf), (x: number) => (('00' + x.toString(16)).slice(-2))).join(''));
 
-  return outerResult.toString();
+  return hexToUint8Array(outerResult.toString());
 }
 
 export async function sumSHA256(input: Uint8Array): Promise<string> {
